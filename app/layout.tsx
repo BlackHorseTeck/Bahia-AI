e21621efeb56646abb5ddc68a51a52b32f201a03
BlackHorseTeck <147159669+BlackHorseTeck@users.noreply.github.com>
2024-10-09 19:49:00 +0100

    1.5

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,19 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
-import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ClerkProvider } from "@clerk/nextjs";
 
-const IBMPlex = IBM_Plex_Sans({ 
+import "./globals.css";
+
+const IBMPlex = IBM_Plex_Sans({
   subsets: ["latin"],
   weight: ['400', '500', '600', '700'],
   variable: '--font-ibm-plex'
 });
 
-
 export const metadata: Metadata = {
-  title: "Bahia AI",
-  description: "Make your Imagination True",
+  title: "Imaginify",
+  description: "AI-powered image generator",
 };
 
 export default function RootLayout({
@@ -25,12 +25,11 @@ export default function RootLayout({
     <ClerkProvider appearance={{
       variables: { colorPrimary: '#624cf5' }
     }}>
-    <html lang="en">
-      <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
+      <html lang="en">
+        <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
           {children}
-      </body>
-    </html>
-  </ClerkProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
-
